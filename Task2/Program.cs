4fe7769abb6cf22b1e58bf315395d5a72c60f061
Polina <ms.maylin@inbox.ru>
2023-02-14 18:19:43 +0300

    In task2: Akkerman renamed to AckermannFunction

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -3,11 +3,11 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
-int Akkerman(int m, int n)
+int AckermannFunction(int m, int n)
 {
     if (m == 0) return n + 1;
-    else if (m > 0 && n == 0) return Akkerman(m - 1, 1);
-    else if (m > 0 && n > 0) return Akkerman(m - 1, Akkerman(m, n - 1));
+    else if (m > 0 && n == 0) return AckermannFunction(m - 1, 1);
+    else if (m > 0 && n > 0) return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
     else return 0;
 }
 
@@ -16,6 +16,6 @@ int M = rand.Next(0, 4);
 int N = rand.Next(0, 4);
 if (M >= 0 && N >= 0)
 {
-    Console.WriteLine($"Функция Аккермана с аргументами {M} и {N} = {Akkerman(M, N)}.");
+    Console.WriteLine($"Функция Аккермана с аргументами {M} и {N} = {AckermannFunction(M, N)}.");
 }
 else Console.WriteLine($"Нельзя вычислить функцию Аккермана, поскольку входные числа {M} и {N} меньше нуля.");
